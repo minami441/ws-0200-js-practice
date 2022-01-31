d016b39d3410fc0eae300224d198436411b77572
minami <sadayamamirai@jouzanmirainoMacBook-Pro.local>
2022-01-31 22:28:34 +0900

    Update

--- 004_algorithm/003_sort.js
+++ 004_algorithm/003_sort.js
@@ -9,6 +9,17 @@
  */
 
 function bubbleSort (array) {
+
+  for(let i=0;i<array.length;i++){
+    for(let p=0;p<array.length;p++){
+      if(array[i]<array[p]){
+        var tmp=array[i]
+        array[i]=array[p]
+        array[p]=tmp
+      }
+    }
+  }
+
   return array
 }
 
@@ -23,6 +34,19 @@ function bubbleSort (array) {
  */
 
 function insertSort (array) {
+  for(let i=0;i<array.length;i++){
+    for(let p=0;p<i;p++){
+      if(array[i]<array[p]){
+        var tmp=i
+        var box=array[i]
+        while(p<=tmp){
+          tmp--
+          array[tmp+1]=array[tmp]
+        }
+        array[tmp+1]=box
+        }
+      }
+    }
   return array
 }
 
@@ -37,10 +61,36 @@ function insertSort (array) {
  */
 
 function mergeSort (arr) {
-  return arr
+
+  var num=Math.floor(arr.length/2)
+  var left=arr.slice(0,num)
+  var right=arr.slice(num)
+  
+  arr=merge(left,right)
+    return arr
+
 }
 
 function merge(left, right) {
+  if(left.length>1){
+    var num=Math.floor(left.length/2)
+    var left_left=left.slice(0,num)
+    var left_right=left.slice(num)
+
+    var left=merge(left_left,left_right)
+}
+if(right.length>1){
+    var num=Math.floor(right.length/2)
+    var right_left=right.slice(0,num)
+    var right_right=right.slice(num)
+    var right=merge(right_left,right_right)
+
+}
+
+var arr=left.concat(right)
+arr.sort((a, b) => a - b)
+
+return arr
 }
 
 /**
